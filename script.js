e76aa205020ed992907b9dfb5d74ddb14ece81a1
Ayaan Shaikh <ayaanmaths04@gmail.com>
2023-09-29 19:31:04 +0530

    update

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ menu_toggler.addEventListener('click', () => {
 	document.body.classList.toggle('nav-open');
 	menu_toggler.classList.toggle('active');
 });
+
+const nav_links = document.querySelectorAll('.nav-link');
+nav_links.forEach((link, index) => {
+	link.style.setProperty('--delay', `${(index * .4 + 2)}s`);
+});
