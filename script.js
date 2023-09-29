e63ea9ecb379d320ac334548eba29425119c9772
Ayaan Shaikh <ayaanmaths04@gmail.com>
2023-09-29 19:03:16 +0530

    Fix

--- script.js
+++ script.js
@@ -2,4 +2,5 @@ const menu_toggler = document.querySelector('.menu-toggler');
 
 menu_toggler.addEventListener('click', () => {
 	document.body.classList.toggle('nav-open');
+	menu_toggler.classList.toggle('active');
 });
